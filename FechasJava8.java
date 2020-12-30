211b8850e4e052b580b558b081afcbd34e96cb85
samuel <samubp10@gmail.com>
2020-12-30 18:15:49 +0100

    Revert "Ejemplo clase MonthDay"
    This reverts commit 67ee45f6529a97ccb0dabfeb7e331086a1684401.
    
    # Conflicts:
    #	FechasJava8.java

--- FechasJava8.java
+++ FechasJava8.java
@@ -7,12 +7,8 @@ package javatime;
 
 // Clase para tratar fechas (sólo día, mes y año)
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 // Enumerado para los meses del año (Enero, ...)
 import java.time.Month;
-import java.time.MonthDay;
-import java.time.Period;
-import java.time.YearMonth;
 
 /**
  *
@@ -50,28 +46,7 @@ public class FechasJava8 {
         
         // Método isBefore()
         
-        /* EJEMPLO CON LA CLASE MONTHDAY */
-        // Sólo guarda el día y el mes (no el año)
-        LocalDate diaAnda = LocalDate.of(1976, Month.FEBRUARY, 28);
-        
-        MonthDay diaMesAndalucia = MonthDay.of(diaAnda.getMonth(), diaAnda.getDayOfMonth());
-        MonthDay diaMesHoy = MonthDay.from(LocalDate.now());
-        
-        if (diaMesAndalucia.equals(diaMesHoy)){
-            System.out.println("Hoy es el día de Andalucía");
-            
-        }
-         
-        /* EJEMPLO CON LA CLASE YEARMONTH */
-        // Sólo guarda el año y el mes (no el día)
-        
-        YearMonth caducidadTarjeta = YearMonth.of(2019, Month.MARCH);
-        System.out.println("Su tarjeta caduca en " + caducidadTarjeta);
-        YearMonth actual = YearMonth.from(LocalDate.now());
-        
-        if (caducidadTarjeta.isBefore(actual)){
-            System.out.println("Su tarjeta ha caducado");
-        }
+        /* EJEMPLO CON LA CLASE MOTHDAY */
         
         
     }
